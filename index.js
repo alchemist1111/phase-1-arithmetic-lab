ee393a27b70f02f32cfb0b14d987e6c2b28c1888
eugene-emoyo-ojakaa <emoyoeugine@gmail.com>
2024-03-18 21:47:31 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1;
+let num3 = 22;
+let num4 = 6;
+let mod = num3 % num4;
+let numbers = [10, 5, 20, 15];
+let max = Math.max(...numbers);
